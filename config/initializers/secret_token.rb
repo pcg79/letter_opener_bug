19d30ad382c82217b286daf0c50102ba8344569e
Pat George <pat.george@gmail.com>
2011-09-23 16:53:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetterOpenerBug::Application.config.secret_token = 'f14d6c7ceea772ccba0a65419f541fb5866f23d86f942a16c4790dbebe1b1f72ca0507da96f37b6eda47ef0cc918c05cf1401dcf1ab122396a6eb234201abb06'
